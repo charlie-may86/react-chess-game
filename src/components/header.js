081ddaff9eb86058d45d44549827277b78bf2d94
Charles May <charlie@Charless-MacBook-Pro.local>
2021-04-05 09:54:45 -0500

    app is working

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    border: 3px black solid;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+`;
+
+export default function header() {
+  return (
+    <StyledHeader>
+      <h1>Around the Board</h1>
+      <h2>A Chess Board Memorization Game</h2>
+    </StyledHeader>
+  );
+}
